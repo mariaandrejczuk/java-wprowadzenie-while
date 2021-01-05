7b7dc0881194b240cc229ccc0fcbeb9376473b5e
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-01-05 13:28:35 +0100

    #5 utworzenie pliku ChoinkaAsymetryczna.java i początek kodu

---
+++ src/ChoinkaAsymetryczna.java
@@ -0,0 +1,8 @@
+public class ChoinkaAsymetryczna {
+    public static void main(String[] args) {
+        int n = 4;
+        while(n < 5) {
+            System.out.println("*");
+        }
+    }
+}
